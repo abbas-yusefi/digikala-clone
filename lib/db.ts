d05914a9972fbe52cd9bc8ad4b4130b0d7acc6ba
abbas yusefi <abbas.a.yusefi@gmail.com>
2026-09-02 16:02:09 +0330

    feat: using neondb postgresql database for delpoyement

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,22 @@
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+// });
+
+// export default pool;
+
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // needed for Neon
+  },
 });
 
 export default pool;
