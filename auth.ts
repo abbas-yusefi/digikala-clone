3303c40745394c0962744b024770cd871a598b11
abbas yusefi <abbas.a.yusefi@gmail.com>
2026-09-05 11:18:09 +0330

    fix: Use bcryptjs for Cloudflare compatibility

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import pool from "./lib/db";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   trustHost: true,
